2d099ef32f3ecd3a277a3675461767068f7efe47
Peter Savichev (proton) <psavichev@gmail.com>
2017-11-05 20:56:34 +0300

    it works!

--- lib/address_parser.rb
+++ lib/address_parser.rb
@@ -1,17 +1,53 @@
 require 'geocoder'
 
 class AddressParser
-  attr_reader :address
+  attr_reader :address, :result, :error, :geo
 
   def initialize(address)
-    @address = address
+    @address = address.to_s.strip
   end
 
   def parse
-    # Geocoder.search(address).first&.address.to_json
+    return finish_with_error('no address') if address.empty?
+
+    @geo = Geocoder.search(address).first
+    return finish_with_error('not found') unless geo
+
+    true
+  end
+
+  def successful?
+    error.nil?
   end
 
   def result
-    {:error=>"no address"}
+    if error
+      { error: error }
+    else
+      geo_result
+    end
+  end
+
+  private
+
+  def finish_with_error(error)
+    @error = error
+    false
+  end
+
+  def geo_result
+    route_info = geo.address_components_of_type(:route).first
+    street = route_info['long_name'] if route_info
+    sublocality_info = geo.address_components_of_type(:sublocality).first
+    district = sublocality_info['long_name'] if sublocality_info
+
+    @result = {
+      street_number: geo.street_number,
+      street: street,
+      district: district,
+      city: geo.city,
+      state: geo.state,
+      country: geo.country
+    }
   end
 end
